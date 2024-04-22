825b98727e5306d2b6f8f74c0413390c978e9cdf
Vasil <bumblebee47293@gmail.com>
2024-04-23 00:00:43 +0530

    scaffold user model files

--- medium_clone/test/medium_clone_web/controllers/error_html_test.exs
+++ medium_clone/test/medium_clone_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule MediumCloneWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(MediumCloneWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(MediumCloneWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
---
+++ medium_clone/test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,24 @@
+defmodule MediumClone.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `MediumClone.Accounts` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        email: "some email",
+        first_name: "some first_name",
+        last_name: "some last_name",
+        password_hash: "some password_hash",
+        role: "some role"
+      })
+      |> MediumClone.Accounts.create_user()
+
+    user
+  end
+end
